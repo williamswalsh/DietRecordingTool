ae205eed1e1ffaa0fae4a38b8c868149aa999d31
William Walsh <william.stephen.walsh@gmail.com>
2018-05-14 15:55:30 +0100

    Update Main.java

--- DietRecordingProject/src/main/Main.java
+++ DietRecordingProject/src/main/Main.java
@@ -4,7 +4,7 @@ import mvc.M;
 import mvc.V;
 import mvc.C;
 /**
- * Title: Duke’s 2000 Calorie Daily Diet
+ * Title: Dukeâ€™s 2000 Calorie Daily Diet
  * 
  * This is the Main Class of the DietRecording project.
  * This class contains the main program flow.
@@ -38,24 +38,3 @@ public class Main {
 		}
 	}
 }
-
-/*
- * private boolean setMenuMeals() { // Can implement hibernate solution here to
- * persist data to database using
- * 
- * 
- * class Meals{
- * 
- * //Model.getSelectedMeals().sumOfCalories(); } // Potentially build class
- * which holds arraylist of Meals and processes them // Finds sum of calories //
- * Data Access Object
- * 
- * // could do this in controller instance constructor -> controller as an
- * instance // (Singleton)
- * 
- * // public boolean isSumOfMealsWithinMax(ArrayList<Meal> selectedMeals, int //
- * indexOfSelectedMeal) { // return isWithinMax; // } // public static boolean
- * isFoodItemSelectable(int mealNumber) { // return false; // }
- * 
- * 
- */
